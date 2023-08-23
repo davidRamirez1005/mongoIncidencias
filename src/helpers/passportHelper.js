7b5b8da1641df970dfed1c8d08b2c4565c820ca3
santiago <santiago.gonzalezagudelo@gmail.com>
2023-08-23 14:03:41 -0500

    feat: :safety_vest: validacion passport

---
+++ src/helpers/passportHelper.js
@@ -0,0 +1,12 @@
+import passport from "passport";
+import { Strategy as  BearerStrategy} from "passport-http-bearer";
+import { validateToken } from "../Auth/jwt/validarToken.js";
+
+passport.use(new BearerStrategy(
+        async function(token, done) {
+        const usuario =  await validateToken(token)
+        if (!usuario) return done(null, false); // No se encontró el token en la colección token o el token no es válido
+        return done(null, usuario); // El token es válido y se agrega el documento de la colección token a req.user
+    }
+));
+export default passport;
